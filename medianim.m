178a08dbf69bc53f8ac8145740cd7a24363b543d
Omar Masri <o.masri18@gmail.com>
2023-12-20 13:32:13 +0100

    commit disperata

---
+++ medianim.m
@@ -0,0 +1,19 @@
+function [outim] = medianim(annotationsFileName)
+    arguments 
+        annotationsFileName string; 
+    end
+    
+    cocoTrainData = jsondecode(fileread(annotationsFileName));
+    images = cocoTrainData.images;
+    annotations = struct2table(cocoTrainData.annotations);
+    categories = struct2table(cocoTrainData.categories);
+
+    for k1 = 1:length(images)
+        imagestruct = images(k1);
+        im = imread(imagestruct.path);
+
+        X(:,:,:,k1) = im;
+    end
+
+    outim = median(X,4);
+end
